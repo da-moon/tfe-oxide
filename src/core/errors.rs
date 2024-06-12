82185b7d4f15c287b05c530e8eb982212ff219fe
da-moon <contact@havi.dev>
2024-06-11 22:31:32 -0400

    feat(core): init `errors`
    Signed-off-by: da-moon <contact@havi.dev>
    
    Precommit-Verified: 7eafb812fcabe1096528011b3760c2026592b263a54fbfbbb4b34c6a2afc4f84

---
+++ src/core/errors.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+
+/// Custom enum that contains all the possible errors that may occur when making
+/// API requests.
+#[derive(Debug, PartialEq, Eq, thiserror::Error, miette::Diagnostic)]
+pub enum Error {
+    /// The request was made, but the server returned an unsuccessful status
+    /// code, such as 404 or 503. In some cases, the response may contain a
+    /// error detail from the server.
+    #[error("`{canonical_reason}`")]
+    #[diagnostic(code(core::response))]
+    Response {
+        canonical_reason: String,
+        status: Option<String>,
+        body: Option<serde_json::Value>,
+    },
+}
+// ────────────────────────────────────────────────────────────
+// vim: filetype=rust syntax=rust softtabstop=4 tabstop=4 shiftwidth=4 textwidth=79 fileencoding=utf-8 expandtab
+// code: language=rust insertSpaces=true tabSize=4
