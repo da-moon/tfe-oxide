19b5a3293f879e6e96e2bbaefc1d7c7ecc21a7e7
da-moon <contact@havi.dev>
2024-06-14 19:33:10 -0400

    feat(jsonapi): `Pagination` struct
    Refs: #8
    Signed-off-by: da-moon <contact@havi.dev>
    
    Precommit-Verified: 50b61288fc115289a4657758516eedbaf899fbfc342a0409f10191d021e08063

--- src/jsonapi/mod.rs
+++ src/jsonapi/mod.rs
@@ -5,6 +5,8 @@
 /// This module contains objects that make up a successful JSON:API response.
 pub mod success;
 
+pub type ListOptions = success::ListOptions;
+pub type Pagination = success::Pagination;
 // ────────────────────────────────────────────────────────────
 // vim: filetype=rust syntax=rust softtabstop=4 tabstop=4 shiftwidth=4 textwidth=79 fileencoding=utf-8 expandtab
 // code: language=rust insertSpaces=true tabSize=4
--- src/jsonapi/success.rs
+++ src/jsonapi/success.rs
@@ -13,7 +13,23 @@ use {
 )]
 #[getset(get = "pub with_prefix")]
 #[serde(rename_all = "kebab-case")]
-pub(crate) struct ListOptions {
+pub struct ListOptions {
+    current_page: u32,
+    total_pages: u32,
+    total_count: u32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    prev_page: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    next_page: Option<u32>,
+}
+/// This struct contains fields that are part of server response for endpoints
+/// that return a list of resources which support pagination.
+#[derive(
+    PartialEq, Debug, Clone, Default, Deserialize, Serialize, Getters,
+)]
+#[getset(get = "pub with_prefix")]
+#[serde(rename_all = "kebab-case")]
+pub struct Pagination {
     current_page: u32,
     total_pages: u32,
     total_count: u32,
