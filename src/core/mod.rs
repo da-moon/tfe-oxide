6ba97760cf70d2fda28a4797f5473a785b167d23
da-moon <contact@havi.dev>
2024-06-14 18:03:16 -0400

    feat(core): implement `HttpClient` trait
    Refs: #2
    Signed-off-by: da-moon <contact@havi.dev>
    
    Precommit-Verified: 382bc5665b2ab0ae94a29a192ac8006a378b9de3b6497707204bf7cde6363557

---
+++ src/core/mod.rs
@@ -0,0 +1,91 @@
+/// This module implements possible error message(s) that might occur when
+/// making API calls.
+// TODO: maybe this should be private
+pub mod errors;
+// ────────────────────────────────────────────────────────────
+use {
+    serde::{de::DeserializeOwned, Serialize},
+    std::fmt::Debug,
+    std::marker::{Send, Sync},
+};
+// ────────────────────────────────────────────────────────────
+/// This represents a error in this library.
+pub type Error = errors::Error;
+/// This represents client headers.
+pub type Headers = std::collections::HashMap<String, String>;
+/// This represents query section of an HTTP request.
+pub type Query<'a> = std::collections::HashMap<&'a str, &'a str>;
+// ────────────────────────────────────────────────────────────
+/// This trait represents the interface to be implemented for an HTTP client,
+/// which is kept separate from the implementation.
+///
+/// When a request doesn't need to pass parameters, the empty or default value
+/// of the payload type should be passed, like `json!({})` or `Query::new()`.
+/// This avoids using `Option<T>` because `serde_json::Value` itself may be null in other
+/// different ways (`serde_json::Value::Null`, an empty `serde_json::Value::Object`...), so this removes
+/// redundancy and edge cases (a `Some(serde_json::Value::Null), for example, doesn't make
+/// much sense).
+#[maybe_async::maybe_async]
+pub trait HttpClient: Send + Clone + Debug {
+    /// sends GET request
+    async fn get<R, S>(
+        &self,
+        url: S,
+        headers: Option<&Headers>,
+        payload: Option<&Query>,
+    ) -> Result<R, Error>
+    where
+        R: DeserializeOwned + Serialize + Debug,
+        S: AsRef<str> + Sync + Send;
+
+    /// sends POST request
+    async fn post<R, S, T>(
+        &self,
+        url: S,
+        headers: Option<&Headers>,
+        payload: T,
+    ) -> Result<R, Error>
+    where
+        R: DeserializeOwned + Serialize + Debug,
+        S: AsRef<str> + Sync + Send,
+        T: Serialize + Debug + Send + Sync;
+
+    /// sends PUT request
+    async fn put<R, S, T>(
+        &self,
+        url: S,
+        headers: Option<&Headers>,
+        payload: T,
+    ) -> Result<R, Error>
+    where
+        R: DeserializeOwned + Serialize + Debug,
+        S: AsRef<str> + Sync + Send,
+        T: Serialize + Debug + Send + Sync;
+
+    /// sends PATCH request
+    async fn patch<R, S, T>(
+        &self,
+        url: S,
+        headers: Option<&Headers>,
+        payload: T,
+    ) -> Result<R, Error>
+    where
+        R: DeserializeOwned + Serialize + Debug,
+        S: AsRef<str> + Sync + Send,
+        T: Serialize + Debug + Send + Sync;
+
+    /// sends DELETE request
+    async fn delete<R, S, T>(
+        &self,
+        url: S,
+        headers: Option<&Headers>,
+        payload: T,
+    ) -> Result<R, Error>
+    where
+        R: DeserializeOwned + Serialize + Debug,
+        S: AsRef<str> + Sync + Send,
+        T: Serialize + Debug + Send + Sync;
+}
+// ────────────────────────────────────────────────────────────
+// vim: filetype=rust syntax=rust softtabstop=4 tabstop=4 shiftwidth=4 textwidth=79 fileencoding=utf-8 expandtab
+// code: language=rust insertSpaces=true tabSize=4
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,3 @@
-mod core;
+/// This module implements a basic HTTP client that can be used to send requests to a server.
+// TODO: maybe this should be private
+pub mod core;
